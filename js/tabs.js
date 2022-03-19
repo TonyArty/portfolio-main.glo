c02eaefbfc32c44814d6111f26deda132e7c116c
TonyArty <i.openuser@gmail.com>
2022-03-19 22:46:37 +0500

    tabDescription, tabImages

--- js/tabs.js
+++ js/tabs.js
@@ -1,8 +1,33 @@
 const tabButtons = document.querySelectorAll('.design-list__item');
-console.dir(tabButtons);
+const tabDescription = document.querySelectorAll('.design__descr');
+const tabImages = document.querySelectorAll('.design-images');
 
 tabButtons.forEach((tabButton, index) => {
   tabButton.addEventListener('click', (event) => {
-    console.dir(event.target);
+    tabButton.dataset.active = '123'
+    tabDescription.forEach((descr, indexDescr) => {
+      if(index === indexDescr) {
+        descr.classList.remove('hidden');
+      } else {
+        descr.classList.add('hidden');
+      }
+    })
+    tabImages.forEach((images, indeximages) => {
+      if(index === indeximages) {
+        images.classList.remove('hidden');
+      } else {
+        images.classList.add('hidden');
+      }
+    })
+    tabButtons.forEach((btn) => {
+      if(btn === event.target) {
+        btn.classList.add('design-list__item_active');
+      } else {
+        btn.classList.remove('design-list__item_active');
+      }
+    })
   })
 })
+
+
+// Day 1: 38:30
