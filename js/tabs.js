6ad1861a805a9c644aea497f7cad04eaeeb385b1
TonyArty <i.openuser@gmail.com>
2022-03-20 23:35:34 +0500

    designImages

--- js/tabs.js
+++ js/tabs.js
@@ -1,6 +1,8 @@
 const tabButtons = document.querySelectorAll('.design-list__item');
 const tabDescription = document.querySelectorAll('.design__descr');
 const tabImages = document.querySelectorAll('.design-images');
+const designImages = document.querySelectorAll('.design-block__img');
+// const designTitle = document.querySelectorAll('.design__title');
 
 const changeContent = (array, value) => {
   array.forEach((elem) => {
@@ -17,6 +19,15 @@ tabButtons.forEach((tabButton) => {
     const dataValue = tabButton.dataset.tabsHandler;
     changeContent(tabDescription, dataValue);
     changeContent(tabImages, dataValue);
+    changeContent(designImages, dataValue);
+
+    // designTitle.forEach((title) => {
+    //   if(title === event.target) {
+    //     title.classList.add('hidden');
+    //   } else {
+    //     title.classList.remove('hidden');
+    //   }
+    // })
 
     tabButtons.forEach((btn) => {
       if(btn === event.target) {
