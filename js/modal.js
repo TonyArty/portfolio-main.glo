3e3b4c3c8114a34612a7e8baddfe0008decd67f1
TonyArty <i.openuser@gmail.com>
2022-03-21 18:16:06 +0500

    modal, moreButton, overlay, closeButton

---
+++ js/modal.js
@@ -0,0 +1,16 @@
+const moreButton = document.querySelector('.more');
+const modal = document.querySelector('.modal');
+const overlay = modal.querySelector('.overlay');
+const closeButton = modal.querySelector('.modal__close');
+
+moreButton.addEventListener('click', () => {
+  modal.classList.remove('hidden');
+})
+
+overlay.addEventListener('click', () => {
+  modal.classList.add('hidden');
+})
+
+closeButton.addEventListener('click', () => {
+  modal.classList.add('hidden');
+})
